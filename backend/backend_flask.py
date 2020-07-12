51a49bfd5f3b7a0b83201ee2fed776572dde9960
sanjit1995 <sonu.sanjit@gmail.com>
2020-07-12 16:08:39 +0530

    fixing bugs

--- backend/backend_flask.py
+++ backend/backend_flask.py
@@ -15,7 +15,7 @@ def render_react():
 # Define function for /getRating route
 @app.route('/getRating', methods=['POST'])
 def getRating():
-    print(request)
+    print(request.json)
     with open('/app/backend/models/scaler_model_from_pickle1.pkl', 'rb') as fs:
         scaler = pickle.load(fs)
     with open('/app/backend/models/keeper_model_svr_from_pickle1.pkl', 'rb') as fk:
