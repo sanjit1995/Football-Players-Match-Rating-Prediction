28384cb90bf8fdcb4e1b573eba37e6bf4e637741
sanjit1995 <sonu.sanjit@gmail.com>
2020-07-12 17:17:45 +0530

    fixing bugs

---
+++ backend/build/precache-manifest.9b86f2e52112357d52e72b95a4542b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd1a8e3958523da4d7c4148c837b33a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4dd77a52e0d53f25767",
+    "url": "/static/css/main.1397f5e1.chunk.css"
+  },
+  {
+    "revision": "c6c93066d511e6312a48",
+    "url": "/static/js/2.f09336b4.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/static/js/2.f09336b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4dd77a52e0d53f25767",
+    "url": "/static/js/main.987b9c38.chunk.js"
+  },
+  {
+    "revision": "02aee2e160a50fb3fa1e",
+    "url": "/static/js/runtime-main.bcc02163.js"
+  }
+]);
